203e1d6bd16737d6f584521e66c65c331bc61720
skuhlmann <samkuhlmann@gmail.com>
2022-01-06 10:29:45 -0700

    link fix

--- src/components/daohausLink.jsx
+++ src/components/daohausLink.jsx
@@ -6,12 +6,11 @@ import { RiExternalLinkLine } from 'react-icons/ri';
 const DaohausLink = ({ linkText, project, route }) => {
   const { daoid, daochain } = useParams();
 
+  const url = `https://app.daohaus.club/dao/${project?.networkID ||
+    daochain}/${project?.id || daoid}${route || ''}`;
+
   return (
-    <Link
-      href={`https://app.daohaus.club/dao/${project?.networkID ||
-        daochain}/${project?.id || daoid}${route && route}`}
-      isExternal
-    >
+    <Link href={url} isExternal>
       <Flex align='center'>
         {linkText}
         <Icon ml={2} as={RiExternalLinkLine} name='transaction link' />
