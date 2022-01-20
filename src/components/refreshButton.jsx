53950c38f0583a3a163ec0237328791c34234213
skuhlmann <samkuhlmann@gmail.com>
2022-01-20 08:15:56 -0700

    refetch user memberships in button

--- src/components/refreshButton.jsx
+++ src/components/refreshButton.jsx
@@ -2,24 +2,23 @@ import React, { useState } from 'react';
 import { IconButton } from '@chakra-ui/button';
 import { RiRefreshLine } from 'react-icons/ri';
 import { Spinner } from '@chakra-ui/spinner';
+
 import { useDao } from '../contexts/DaoContext';
+import { useUser } from '../contexts/UserContext';
 
 const RefreshButton = () => {
   const { refetch } = useDao();
+  const { refetchUserMemberships } = useUser();
   const [fetching, setFetching] = useState(false);
 
   const handleRefetch = () => {
     setFetching(true);
     setTimeout(() => {
       refetch();
+      refetchUserMemberships();
       setFetching(false);
-    }, 2000);
+    }, 3000);
   };
-
-  // const turnOff = true;
-  // if (turnOff) {
-  //   return null;
-  // }
 
   if (fetching) {
     return <Spinner ml={3} color='secondary.500' />;
